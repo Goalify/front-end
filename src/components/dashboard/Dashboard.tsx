d4bc7c99a1010c3b9887cce44156e17da2858dfb
hasankhadra <hasankhadra2013@gmail.com>
2021-09-02 22:09:59 +0300

    added Dashboard component

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Dashboard = () => {
+    return <div>Dashboard</div>
+}
+
+export default Dashboard;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dashboard from './components/dashboard/Dashboard';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
