7353cc10709b55e0b69924f586c175a7242103fd
hasankhadra <hasankhadra2013@gmail.com>
2021-09-11 19:36:11 +0300

    Hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
 import Dashboard from './components/dashboard/Dashboard'
 import { propTypes } from 'react-bootstrap/esm/Image';
-import HeaderNavBar from './components/dashboard/HeaderNavBar';
+import HeaderNavBar from './components/HeaderNavBar';
 import Footer from './components/common/Footer';
-import PlatformRouter from './components/PlatformRouter'
 import GoalsList from "./components/dashboard/GoalsList"
 import {Goals} from "./tsInterfaces/Goals"
+import Login from './components/account_setup/Login';
 
 function App(props: Goals){
 
     return <div>
-            <HeaderNavBar list={props.list}/>
-            <Footer />
+            {/* <HeaderNavBar list={props.list}/>
+            <Footer /> */}
+            <Login />
         </div>
 
 }
--- src/components/dashboard/test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-
-
-function Test(){
-
-}
-
-export default Test;
